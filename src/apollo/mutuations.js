659d01ee60b521c0032279a34130f0daecfb10db
alemgena <alemgenateferi1@gmil.com>
2024-09-23 12:16:57 +0300

    fix role attach

--- src/apollo/mutuations.js
+++ src/apollo/mutuations.js
@@ -950,7 +950,7 @@ export const UPDATE_USER = gql`
 
 //ROles
 export const ATTACH_ROLE = gql`
-  mutation ($user_id: ID!, $role: RoleEnum!) {
+  mutation ($user_id: ID!, $role: String!) {
     attachRole(user_id: $user_id, role: $role) {
       id
       name
